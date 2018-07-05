cd7a513e9bee017d6bd6a8df109409fe97a366fe
Entrant <trueentrant@gmail.com>
2018-07-05 11:21:19 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>Thats all that i can add to this page</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, ipsam. Facere 
+          doloremque ab quos recusandae voluptatibus quae corporis vel, consectetur quod 
+          ipsum minus, possimus dolore! Facilis tempora incidunt ad alias?</p>
       </div>
     );
   }
